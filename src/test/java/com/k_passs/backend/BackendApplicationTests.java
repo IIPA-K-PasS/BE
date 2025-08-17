132773c94709aee8d1fa2169c102394d88a6d083
Handher0 <handher0@naver.com>
2025-08-17 16:33:50 +0900

    feat: 프로젝트 세팅

---
+++ src/test/java/com/k_passs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k_passs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
